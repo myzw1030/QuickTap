0f293ae5bddfe3ee703b8e083fffc01f7422267c
tommy <tmtmtoa@gmail.com>
2023-01-05 20:29:36 +0900

    ファイル追加

---
+++ app/src/main/java/com/example/quicktap/GameAction.java
@@ -0,0 +1,14 @@
+package com.example.quicktap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameAction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_action);
+    }
+}
